8b5486c1949013f8d26c7d827933834e7268bcea
mjesbar <miguedjba96@gmail.com>
2025-08-07 12:26:15 -0500

    fix: vertical space between elements at start page

--- plugins/owp/pages/start/start.js
+++ plugins/owp/pages/start/start.js
@@ -14,27 +14,30 @@ class OwpStart extends HTMLElement {
         shadowRoot.innerHTML = `
             <link rel="stylesheet" href="${window.location.origin}/wp-content/plugins/owp/assets/css/output.css">
             
-            <owp-top-bar></owp-top-bar>
-
-            <div class="bg-white p-12 rounded-lg shadow-md max-w-3xl mx-auto mt-10">
-                <h2 class="text-2xl font-bold mb-4">
-                    Let's build your website!
-                </h2>
-                <p class="text-md text-gray-600 my-6">
-                    Please share some basic details of the website to get started.
-                </p>
-
-                <owp-start-name-field></owp-start-name-field>
-
-                <div class="flex flex-col md:flex-row justify-between gap-6 my-6">
-                    <owp-start-business-selector></owp-start-business-selector>
-                    <owp-start-language-selector></owp-start-language-selector>
+            <div class="h-screen bg-purple-50">
+                <owp-top-bar></owp-top-bar>
+    
+                <div class="bg-white p-12 rounded-lg shadow-md max-w-3xl mx-auto mt-10">
+                    <h2 class="text-2xl font-bold mb-4">
+                        Let's build your website!
+                    </h2>
+                    <p class="text-md text-gray-600 my-6">
+                        Please share some basic details of the website to get started.
+                    </p>
+    
+                    <owp-start-name-field></owp-start-name-field>
+    
+                    <div class="flex flex-col md:flex-row justify-between gap-6 my-6">
+                        <owp-start-business-selector></owp-start-business-selector>
+                        <owp-start-language-selector></owp-start-language-selector>
+                    </div>
+    
+                    <div class="flex justify-between my-6">
+                        <owp-back-button redirect-to="/wp-admin/"></owp-back-button>
+                        <owp-next-button redirect-to="/wp-admin/admin.php?page=owp-describe">Next</owp-next-button>
+                    </div>
                 </div>
 
-                <div class="flex justify-between my-6">
-                    <owp-back-button redirect-to="/wp-admin/"></owp-back-button>
-                    <owp-next-button redirect-to="/wp-admin/admin.php?page=owp-describe">Next</owp-next-button>
-                </div>
             </div>
         `;
     }
