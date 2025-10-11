174fb0230bb87c943d21ec2400da933255d7403c
mjesbar <miguedjba96@gmail.com>
2025-10-11 16:06:26 -0500

    fix: apply output.css only to gutenberg, owp-app & previews

--- plugins/owp/owp.php
+++ plugins/owp/owp.php
@@ -74,11 +74,6 @@ add_action( 'admin_init', 'owp_handle_pages_menu_redirect' );
  * @return void
  */
 function owp_render_app_page() {
-    add_filter( 'show_admin_bar', '__return_false' );
-    wp_enqueue_style(
-        'owp-hide-admin-bars',
-        plugins_url( 'assets/css/hide-admin-bars.css', __FILE__ )
-    );
     wp_enqueue_script(
         'owp-app-script',
         plugins_url( 'app/app.js', __FILE__ ),
@@ -96,8 +91,20 @@ function owp_render_app_page() {
  */
 add_action( 'init', 'owp_preview_hide_admin_bar' );
 function owp_preview_hide_admin_bar() {
-    if ( isset( $_GET['owp-preview'] ) && $_GET['owp-preview'] === 'true' ) {
+    if (
+        (
+            isset( $_GET['owp-preview'] ) &&
+            $_GET['owp-preview'] === 'true'
+        ) || (
+            isset( $_GET['page'] ) &&
+            $_GET['page'] === 'owp-app'
+        )
+        ) {
         add_filter( 'show_admin_bar', '__return_false' );
+        wp_enqueue_style(
+            'owp-hide-admin-bars',
+            plugins_url( 'assets/css/hide-admin-bars.css', __FILE__ )
+        );
     }
 }
 
@@ -109,51 +116,59 @@ function owp_preview_hide_admin_bar() {
 * @return void
 */
 function owp_enqueue_components() {
-    // Enqueue global style
-    wp_enqueue_style(
-        'owp-output-style',
-        plugins_url( 'assets/css/output.css', __FILE__ )
-    );
+    // Enqueue global style conditionally
+    if (
+        ( isset( $_GET['page'] ) && $_GET['page'] === 'owp-app' ) ||
+        ( isset( $_GET['action'] ) && $_GET['action'] === 'edit' && basename( $_SERVER['PHP_SELF'] ) === 'post.php' ) ||
+        ( isset( $_GET['post_type'] ) && $_GET['post_type'] === 'page' && basename( $_SERVER['PHP_SELF'] ) === 'post-new.php' )
+    ) {
+        wp_enqueue_style(
+            'owp-output-style',
+            plugins_url( 'assets/css/output.css', __FILE__ )
+        );
 
-    // Enqueue Gutenberg Sidebar scripts
-    wp_enqueue_script(
-        'owp-gutenberg-sidebar',
-        plugins_url( 'gutenberg/sidebar.js', __FILE__ ),
-        array(),
-        null,
-        true
-    );
-
-    // Enqueue Page scripts
-    $page_dir = plugin_dir_path( __FILE__ ) . 'app/pages/';
-    $page_files = glob( $page_dir . '*.js' );
-
-    foreach ( $page_files as $file ) {
-        $handle = 'owp-page-' . sanitize_title( basename( $file, '.js' ) );
-        $src = plugins_url( 'app/pages/' . basename( $file ), __FILE__ );
+        // FIX: this is not working...
+        // Enqueue Gutenberg Sidebar scripts
         wp_enqueue_script(
-            $handle,
-            $src,
+            'owp-gutenberg-sidebar',
+            plugins_url( 'gutenberg/sidebar.js', __FILE__ ),
             array(),
             null,
             true
         );
-    }
+        
 
-    // Enqueue Web Components scripts
-    $component_dir = plugin_dir_path( __FILE__ ) . 'components/';
-    $component_files = glob( $component_dir . '*.js' );
+        // Enqueue Page scripts
+        $page_dir = plugin_dir_path( __FILE__ ) . 'app/pages/';
+        $page_files = glob( $page_dir . '*.js' );
+        
+        foreach ( $page_files as $file ) {
+            $handle = 'owp-page-' . sanitize_title( basename( $file, '.js' ) );
+            $src = plugins_url( 'app/pages/' . basename( $file ), __FILE__ );
+            wp_enqueue_script(
+                $handle,
+                $src,
+                array(),
+                null,
+                true
+            );
+        }
+        
+        // Enqueue Web Components scripts
+        $component_dir = plugin_dir_path( __FILE__ ) . 'components/';
+        $component_files = glob( $component_dir . '*.js' );
 
-    foreach ( $component_files as $file ) {
-        $handle = 'owp-component-' . sanitize_title( basename( $file, '.js' ) );
-        $src = plugins_url( 'components/' . basename( $file ), __FILE__ );
-        wp_enqueue_script(
-            $handle,
-            $src,
-            array(),
-            null,
-            true
-        );
+        foreach ( $component_files as $file ) {
+            $handle = 'owp-component-' . sanitize_title( basename( $file, '.js' ) );
+            $src = plugins_url( 'components/' . basename( $file ), __FILE__ );
+            wp_enqueue_script(
+                $handle,
+                $src,
+                array(),
+                null,
+                true
+            );
+        }
     }
 }
 add_action( 'wp_enqueue_scripts', 'owp_enqueue_components' );
@@ -163,6 +178,7 @@ add_action( 'enqueue_block_editor_assets', 'owp_enqueue_components' );
 
 /**
  * Register plugin API endpoints
+
 * @return void
 */
 function register_api_endpoints() {
