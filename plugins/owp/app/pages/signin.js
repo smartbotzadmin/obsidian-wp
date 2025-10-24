394514a8089970d285e023b047989df1ae0e6fbd
mjesbar <miguedjba96@gmail.com>
2025-10-23 22:34:05 -0500

    feat: complete basic authentication, email, username, password

--- plugins/owp/app/pages/signin.js
+++ plugins/owp/app/pages/signin.js
@@ -19,26 +19,55 @@ class OwpSignin extends HTMLElement {
   connectedCallback() {
     this.className = `flex justify-center items-center h-screen`;
     this.innerHTML = /*html*/`
-      <div class="w-[400px] bg-slate-950 p-8 rounded-3xl border border-slate-700">
-        <h2 class="text-3xl font-bold mb-8 text-slate-100 text-center">Sign In</h2>
-        <form id="signinForm">
-          <div class="mb-4">
-            <label for="login" class="block text-slate-300 text-sm font-semibold mb-2">Email or Username</label>
-            <input type="text" id="login" name="login" class="shadow appearance-none border border-slate-700 rounded-xl w-full h-11 px-3 text-slate-300 text-md bg-slate-900 leading-tight transition-all duration-300 ease-in-out ring ring-0 ring-transparent outline-none hover:ring-1 hover:ring-cyan-500 focus:ring-2 focus:ring-cyan-500" placeholder="Enter your email or username" required>
-          </div>
-          <div class="mb-6">
-            <label for="password" class="block text-slate-300 text-sm font-semibold mb-2">Password</label>
-            <input type="password" id="password" name="password" class="shadow appearance-none border border-slate-700 rounded-xl w-full h-11 px-3 text-slate-300 text-md bg-slate-900 leading-tight transition-all duration-300 ease-in-out ring ring-0 ring-transparent outline-none hover:ring-1 hover:ring-cyan-500 focus:ring-2 focus:ring-cyan-500" placeholder="Enter your password" required>
-          </div>
+      <div class="w-[500px] bg-slate-950 p-8 flex flex-col rounded-3xl border border-slate-700">
+        <h2 class="text-3xl font-bold mb-10 text-slate-100 text-center">
+          Sign In
+        </h2>
+        
+        <form id="signinForm" class="flex flex-col gap-2">
+          
+          <label for="login" class="text-slate-300 text-sm font-semibold">
+            Email or Username
+          </label>
+          <input
+            type="text"
+            id="login"
+            name="login"
+            class="shadow appearance-none border border-slate-700 rounded-xl w-full h-11 px-3 text-slate-300 text-md bg-slate-900 leading-tight transition-all duration-300 ease-in-out ring ring-0 ring-transparent outline-none hover:ring-1 hover:ring-cyan-500 focus:ring-2 focus:ring-cyan-500 mb-6"
+            placeholder="Enter your email or username"
+            required
+            autocomplete="username"
+          >
+          
+          <label for="password" class="text-slate-300 text-sm font-semibold">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            class="shadow appearance-none border border-slate-700 rounded-xl w-full h-11 px-3 text-slate-300 text-md bg-slate-900 leading-tight transition-all duration-300 ease-in-out ring ring-0 ring-transparent outline-none hover:ring-1 hover:ring-cyan-500 focus:ring-2 focus:ring-cyan-500 mb-6"
+            placeholder="Enter your password"
+            required
+            autocomplete="current-password"
+          >
+
           <div class="flex items-center justify-between">
-            <button type="submit" class="bg-cyan-600 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded-xl focus:outline-none focus:shadow-outline transition-all duration-300 ease-in-out">
+            <button id="signinButton" type="submit" class="w-24 h-11 bg-cyan-600 hover:bg-cyan-700 text-white font-semibold text-md py-2 px-4 flex justify-center rounded-xl focus:outline-none focus:shadow-outline transition-all duration-300 ease-in-out cursor-pointer">
               Sign In
             </button>
-            <a href="#signup" class="inline-block align-baseline font-bold text-sm text-cyan-500 hover:text-cyan-800">
-              Don't have an account? Sign Up
-            </a>
+            <div class="flex gap-2 items-center justify-center ">
+              <span class="text-sm font-semibold text-slate-300 cursor-default">
+                Don't have an account?
+              </span>
+              <a href="#signup" class="text-sm font-bold text-cyan-500 hover:text-cyan-800 cursor-pointer transition-all">
+              Sign Up
+              </a>
+            </div>
           </div>
-          <div id="signinMessage" class="mt-4 text-center text-sm font-semibold"></div>
+
+          <div id="signinMessage" class="h-5"></div>
+
         </form>
       </div>
     `;
@@ -64,6 +93,11 @@ class OwpSignin extends HTMLElement {
     const login = this.querySelector('#login').value;
     const password = this.querySelector('#password').value;
     const messageElement = this.querySelector('#signinMessage');
+    const signinButton = this.querySelector('#signinButton');
+
+    signinButton.innerHTML = /*html*/`
+      <img src="/wp-content/plugins/owp/assets/icons/loader.svg" class="animate-spin" />
+    `;
 
     try {
       const response = await fetch('https://obsidian-signin-313065021854.us-east1.run.app', {
@@ -79,16 +113,21 @@ class OwpSignin extends HTMLElement {
       if (response.ok) {
         localStorage.setItem(window.cookieName, data.token);
         messageElement.textContent = 'Signed in successfully!';
-        messageElement.className = 'mt-4 text-center text-sm font-semibold text-green-500';
-        // Redirect or update UI as needed
+        messageElement.className = 'h-5 text-center text-sm font-medium text-green-500';
+        setTimeout(() => {
+          window.location.hash = "start"
+          signinButton.innerHTML = /*html*/`Sign In`;
+        }, 1000)
       } else {
         messageElement.textContent = data.error || data;
-        messageElement.className = 'mt-4 text-center text-sm font-semibold text-red-500';
+        messageElement.className = 'h-5 text-center text-sm font-medium text-red-500';
+        signinButton.innerHTML = /*html*/`Sign In`;
       }
     } catch (error) {
       console.error('Signin Error:', error);
       messageElement.textContent = 'An unexpected error occurred.';
-      messageElement.className = 'mt-4 text-center text-sm font-semibold text-red-500';
+      messageElement.className = 'h-5 text-center text-sm font-medium text-red-500';
+      signinButton.innerHTML = /*html*/`Sign In`;
     }
   }
 }
--- plugins/owp/app/pages/signup.js
+++ plugins/owp/app/pages/signup.js
@@ -17,33 +17,66 @@ class OwpSignup extends HTMLElement {
    * @returns {void}
    */
   connectedCallback() {
-    console.log(window.cookieName)
     this.className = `flex justify-center items-center h-screen`;
     this.innerHTML = /*html*/`
-      <div class="w-[400px] bg-slate-950 p-8 rounded-3xl border border-slate-700">
-        <h2 class="text-3xl font-bold mb-8 text-slate-100 text-center">Sign Up</h2>
-        <form id="signupForm">
-          <div class="mb-4">
-            <label for="email" class="block text-slate-300 text-sm font-semibold mb-2">Email</label>
-            <input type="email" id="email" name="email" class="shadow appearance-none border border-slate-700 rounded-xl w-full h-11 px-3 text-slate-300 text-md bg-slate-900 leading-tight transition-all duration-300 ease-in-out ring ring-0 ring-transparent outline-none hover:ring-1 hover:ring-cyan-500 focus:ring-2 focus:ring-cyan-500" placeholder="Enter your email" required>
-          </div>
-          <div class="mb-4">
-            <label for="username" class="block text-slate-300 text-sm font-semibold mb-2">Username</label>
-            <input type="text" id="username" name="username" class="shadow appearance-none border border-slate-700 rounded-xl w-full h-11 px-3 text-slate-300 text-md bg-slate-900 leading-tight transition-all duration-300 ease-in-out ring ring-0 ring-transparent outline-none hover:ring-1 hover:ring-cyan-500 focus:ring-2 focus:ring-cyan-500" placeholder="Choose a username" required>
-          </div>
-          <div class="mb-6">
-            <label for="password" class="block text-slate-300 text-sm font-semibold mb-2">Password</label>
-            <input type="password" id="password" name="password" class="shadow appearance-none border border-slate-700 rounded-xl w-full h-11 px-3 text-slate-300 text-md bg-slate-900 leading-tight transition-all duration-300 ease-in-out ring ring-0 ring-transparent outline-none hover:ring-1 hover:ring-cyan-500 focus:ring-2 focus:ring-cyan-500" placeholder="Create a password" required>
-          </div>
+      <div class="w-[500px] bg-slate-950 p-8 flex flex-col rounded-3xl border border-slate-700">
+        <h2 class="text-3xl font-bold mb-10 text-slate-100 text-center">
+          Sign Up
+        </h2>
+        
+        <form id="signupForm" class="flex flex-col gap-2">
+          
+          <label for="email" class="text-slate-300 text-sm font-semibold">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            class="shadow appearance-none border border-slate-700 rounded-xl w-full h-11 px-3 text-slate-300 text-md bg-slate-900 leading-tight transition-all duration-300 ease-in-out ring ring-0 ring-transparent outline-none hover:ring-1 hover:ring-cyan-500 focus:ring-2 focus:ring-cyan-500 mb-6"
+            placeholder="Enter your email"
+            required
+          >
+          
+          <label for="username" class="text-slate-300 text-sm font-semibold">
+            Username
+          </label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            class="shadow appearance-none border border-slate-700 rounded-xl w-full h-11 px-3 text-slate-300 text-md bg-slate-900 leading-tight transition-all duration-300 ease-in-out ring ring-0 ring-transparent outline-none hover:ring-1 hover:ring-cyan-500 focus:ring-2 focus:ring-cyan-500 mb-6"
+            placeholder="Choose a username"
+            required
+          >
+          
+          <label for="password" class="text-slate-300 text-sm font-semibold">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            class="shadow appearance-none border border-slate-700 rounded-xl w-full h-11 px-3 text-slate-300 text-md bg-slate-900 leading-tight transition-all duration-300 ease-in-out ring ring-0 ring-transparent outline-none hover:ring-1 hover:ring-cyan-500 focus:ring-2 focus:ring-cyan-500 mb-6"
+            placeholder="Create a password"
+            required
+          >
+
           <div class="flex items-center justify-between">
-            <button type="submit" class="bg-cyan-600 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded-xl focus:outline-none focus:shadow-outline transition-all duration-300 ease-in-out">
+            <button id="signupButton" type="submit" class="w-24 h-11 bg-cyan-600 hover:bg-cyan-700 text-white font-semibold text-md py-2 px-4 flex justify-center rounded-xl focus:outline-none focus:shadow-outline transition-all duration-300 ease-in-out cursor-pointer">
               Sign Up
             </button>
-            <a href="#signin" class="inline-block align-baseline font-bold text-sm text-cyan-500 hover:text-cyan-800">
-              Already have an account? Sign In
-            </a>
+            <div class="flex gap-2 items-center justify-center ">
+              <span class="text-sm font-semibold text-slate-300 cursor-default">
+                Already have an account?
+              </span>
+              <a href="#signin" class="text-sm font-bold text-cyan-500 hover:text-cyan-800 cursor-pointer transition-all">
+                Sign In
+              </a>
+            </div>
           </div>
-          <div id="signupMessage" class="mt-4 text-center text-sm font-semibold"></div>
+
+          <div id="signupMessage" class="h-5"></div>
         </form>
       </div>
     `;
@@ -71,6 +104,11 @@ class OwpSignup extends HTMLElement {
     const username = this.querySelector('#username').value;
     const password = this.querySelector('#password').value;
     const messageElement = this.querySelector('#signupMessage');
+    const signupButton = this.querySelector('#signupButton');
+
+    signupButton.innerHTML = /*html*/`
+      <img src="/wp-content/plugins/owp/assets/icons/loader.svg" class="animate-spin" />
+    `;
 
     try {
       const response = await fetch('https://obsidian-signup-313065021854.us-east1.run.app', {
@@ -85,16 +123,21 @@ class OwpSignup extends HTMLElement {
 
       if (response.ok) {
         messageElement.textContent = data.message;
-        messageElement.className = 'mt-4 text-center text-sm font-semibold text-green-500';
-        this.querySelector('#signupForm').reset();
+        messageElement.className = 'h-5 text-center text-sm font-medium text-green-500';
+        setTimeout(() => {
+          window.location.hash = "signin"
+          this.querySelector('#signupForm').reset();
+          signupButton.innerHTML = /*html*/`Sign Up`;
+        }, 1000)
       } else {
         messageElement.textContent = data.error || data;
-        messageElement.className = 'mt-4 text-center text-sm font-semibold text-red-500';
+        messageElement.className = 'h-5 text-center text-sm font-medium text-red-500';
       }
     } catch (error) {
       console.error('Signup Error:', error);
       messageElement.textContent = 'An unexpected error occurred.';
-      messageElement.className = 'mt-4 text-center text-sm font-semibold text-red-500';
+      messageElement.className = 'h-5 text-center text-sm font-medium text-red-500';
+      signupButton.innerHTML = /*html*/`Sign Up`;
     }
   }
 }
